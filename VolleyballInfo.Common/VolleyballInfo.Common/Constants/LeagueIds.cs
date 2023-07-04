3e02f42208cc550bd54496f7580af30645ebf01e
lmiedzinski <lukasz.miedzinski.1@gmail.com>
2023-07-04 17:36:53 +0200

    Added first message contract and id constants to common project

---
+++ VolleyballInfo.Common/VolleyballInfo.Common/Constants/LeagueIds.cs
@@ -0,0 +1,6 @@
+namespace VolleyballInfo.Common.Constants;
+
+public static class LeagueIds
+{
+    public static int NationsLeagueId => 183;
+}
--- VolleyballInfo.Common/VolleyballInfo.Common/EventBusMessages/NewDataGatheredMessage.cs
+++ VolleyballInfo.Common/VolleyballInfo.Common/EventBusMessages/NewDataGatheredMessage.cs
@@ -1,6 +1,19 @@
 namespace VolleyballInfo.Common.EventBusMessages;
 
-public class NewDataGatheredMessage
-{
-    
-}
+public record NewDataGatheredMessage(
+    int LeagueId,
+    string Season,
+    DateTime GatheredAt,
+    IEnumerable<NewDataGatheredMessageItem> Items);
+
+public record NewDataGatheredMessageItem(
+    int TeamId,
+    string TeamName,
+    int LeaguePosition,
+    int LeaguePoints,
+    string PointsForm,
+    int PlayedGamesCount,
+    int WinGamesCount,
+    int LooseGamesCount,
+    int WinSetsCount,
+    int LooseSetsCount);
